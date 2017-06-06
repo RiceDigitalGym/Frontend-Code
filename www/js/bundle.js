06f0f82b26e66433d9f811771245e28906fda8a5
Rushi Bhalani <rushi.bhalani@gmail.com>
2017-06-06 15:22:09 -0500

    commit webpack change

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -74,7 +74,7 @@ var GLOBALS = {};
 
 
 APP_CONFIG = {
-	API_ENDPOINT : "http://127.0.0.1:8000/bbb"
+	API_ENDPOINT : "http://0.0.0.0:8000/bbb"
 	// API_ENDPOINT : "http://52.34.141.31:8000/bbb"
 }
 
