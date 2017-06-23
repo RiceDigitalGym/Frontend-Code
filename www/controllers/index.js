3fad609c1565072997b44f6125fe8415f0c87b24
JacobBurger <jmb23@rice.edu>
2017-06-23 11:32:25 -0500

    added what I believe may handle login authentication

--- www/controllers/index.js
+++ www/controllers/index.js
@@ -17,5 +17,22 @@ angular.module(GLOBALS.APP_NAME)
 		.controller("AboutController", aboutController)
 		.controller("NavigationController", navigationController)
 		.controller("ProfileController", profileController)
-
-	
+		.factory('authInterceptor', function ($rootScope, $q, $window) {
+		  return {
+		    request: function (config) {
+		      config.headers = config.headers || {};
+		      if (localStorage.token) {
+		        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+		      }
+		      return config;
+		    },
+		    response: function (response) {
+		      if (response.status === 401) {
+		        // handle the case where the user is not authenticated
+		      }
+		      return response || $q.when(response);
+		    }
+		  };
+		}).config(function ($httpProvider) {
+		  $httpProvider.interceptors.push('authInterceptor');
+		});
--- www/controllers/tag.controller.js
+++ www/controllers/tag.controller.js
@@ -2,19 +2,25 @@ TagController.$inject = ['$scope', '$state', 'UserService']
 function TagController($scope, $state, UserService) {
   $scope.checkTag = function() {
   	var iterations = 0
-
-  	while (iterations < 6) {
-  		UserService.checkTag($scope.formData.machineID, $scope.formData.tagName).then(function(response) {
-    		if (response.status == "success") {
-    			return "Tag has been registered!"
-    		} 
-    		else {
-    			setTimeout(function() {
-    				iterations++
-    			}, 5000)    			
-    		}
-    	})
-  	}
+    UserService.checkTag($scope.formData.machineID, $scope.formData.tagName).then(function(response) {
+      if (response.status == "success") {
+         return "Tag has been registered!"
+      } else {
+        return "Tag was not registered!"
+      } 
+    })
+  	// while (iterations < 6) {
+  	// 	UserService.checkTag($scope.formData.machineID, $scope.formData.tagName).then(function(response) {
+   //  		if (response.status == "success") {
+   //  			return "Tag has been registered!"
+   //  		}
+   //  		else {
+   //  			setTimeout(function() {
+   //  				iterations++
+   //  			}, 5000)    			
+   //  		}
+   //  	})
+  	// }
   	$state.go('tab.data')
   }
 }
