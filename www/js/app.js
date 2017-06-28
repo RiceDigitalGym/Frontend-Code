3a438b1ce4b035bd0db9d071f5970b4f16920006
Rushi Bhalani <rushi.bhalani@gmail.com>
2017-06-28 14:04:39 -0500

    Fixed style issues in 'js' folder.

--- www/js/app.js
+++ www/js/app.js
@@ -1,110 +1,112 @@
-console.log("using this file")
+console.log("File 'app.js' is being used.")
 angular.module('DigitalGym', ['ionic', 'chart.js', 'ion-radial-progress'])
+    
+    .run(function($ionicPlatform) {
+        console.log("File 'app.js' is being used. (2)")
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
+    .config(function($stateProvider, $urlRouterProvider, ChartJsProvider, $sceDelegateProvider, $ionicConfigProvider, $httpProvider) {
+        
+        $ionicConfigProvider.views.maxCache(0);
+        $sceDelegateProvider.resourceUrlWhitelist(['self','http://ricedigitalgym.blogs.rice.edu/']);
 
-.run(function($ionicPlatform) {
-  console.log("using this file2")
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
 
-.config(function($stateProvider, $urlRouterProvider, ChartJsProvider, $sceDelegateProvider, $ionicConfigProvider, $httpProvider) {
-  $ionicConfigProvider.views.maxCache(0);
-$sceDelegateProvider.resourceUrlWhitelist(['self','http://ricedigitalgym.blogs.rice.edu/']);
+        $httpProvider.interceptors.push(function($q) {
+            return {
+                'request': function(config) {
+                    console.log("Config: " + JSON.stringify(config));
+                    console.log("Current token: " + localStorage.token);
+                    config.headers['authorization'] = localStorage.token;
+                    console.log(config.headers['authorization']);
+                    return config;
+                }
+            };
+        });
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $httpProvider.interceptors.push(function($q) {
-    return {
-     'request': function(config) {
-          console.log("Config: " + JSON.stringify(config));
-          console.log("Current token: " + localStorage.token);
-          config.headers['authorization'] = localStorage.token;
-          console.log(config.headers['authorization']);
-          return config;
-      }
-    };
-  });
-  $stateProvider
-    .state('login', {
-      url: '/login',
-      templateUrl: 'templates/login.html',
-      controller: "LoginController"
-    })
-    .state('setup', {
-      url: '/setup_account',
-      templateUrl: 'templates/setup.html',
-      controller: "SetupController"
-    })
-    .state('changepassword', {
-      url: '/changepassword',
-      templateUrl: 'templates/changepassword.html',
-      controller: "ChangePasswordController"
-    })
-    .state('tag', {
-      url: '/tag',
-      templateUrl: 'templates/tag.html',
-      controller: "TagController"
-    })
-    .state('tab', {
-      url: '/tab',
-      templateUrl: 'templates/tabs.html',
-      controller: "NavigationController",
-      abstract: true
-    })
-    .state('tab.data',{
-        url: "/data",
-        views: {
-          'tab-data': {
-            templateUrl: "templates/tab-data.html",
-            controller: 'DataController'
-          }
-        }
-    })
-    .state('tab.dash',{
-        url: "/dash",
-        views: {
-          'tab-dash': {
-            templateUrl: "templates/tab-dash.html",
-            controller: 'ProfileController'
-          }
-        }
-    })
-    .state('tab.about',{
-        url: "/about",
-        views: {
-          'tab-about': {
-            templateUrl: "templates/tab-about.html",
-            controller: 'AboutController'
-          }
-        }
-    }) 
-    .state('home', {
-      url: "/home",
-      cache: false,
-      templateUrl: 'templates/home.html',
-      controller: 'HomeController'
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: "LoginController"
+            })
+            .state('setup', {
+                url: '/setup_account',
+                templateUrl: 'templates/setup.html',
+                controller: "SetupController"
+            })
+            .state('changepassword', {
+                url: '/changepassword',
+                templateUrl: 'templates/changepassword.html',
+                controller: "ChangePasswordController"
+            })
+            .state('tag', {
+                url: '/tag',
+                templateUrl: 'templates/tag.html',
+                controller: "TagController"
+            })
+            .state('tab', {
+                url: '/tab',
+                templateUrl: 'templates/tabs.html',
+                controller: "NavigationController",
+                abstract: true
+            })
+            .state('tab.data', {
+                url: "/data",
+                views: {
+                    'tab-data': {
+                        templateUrl: "templates/tab-data.html",
+                        controller: 'DataController'
+                    }
+                }
+            })
+            .state('tab.dash', {
+                url: "/dash",
+                views: {
+                    'tab-dash': {
+                        templateUrl: "templates/tab-dash.html",
+                        controller: 'ProfileController'
+                    }
+                }
+            })
+            .state('tab.about', {
+                url: "/about",
+                views: {
+                    'tab-about': {
+                        templateUrl: "templates/tab-about.html",
+                        controller: 'AboutController'
+                    }
+                }
+            }) 
+            .state('home', {
+                url: "/home",
+                cache: false,
+                templateUrl: 'templates/home.html',
+                controller: 'HomeController'
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/login') 
+
     });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login') 
 
-});
-
-
-require('../services')
+require('../services');
 require('../controllers');
 
 
--- www/js/dev-config.js
+++ www/js/dev-config.js
@@ -2,6 +2,6 @@
 //var APP_CONFIG = {API_ENDPOINT : "http://52.34.141.31:8000/bbb"}
 
 //Development
-var APP_CONFIG = {API_ENDPOINT : "http://localhost:8000/bbb"}
+var APP_CONFIG = {API_ENDPOINT : "http://localhost:8000/bbb"};
 
-module.exports = API_ENDPOINT
+module.exports = API_ENDPOINT;
