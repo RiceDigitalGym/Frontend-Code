b2174e11637990fb3c74c3f3b480452211a6ca87
Rushi Bhalani <rushi.bhalani@gmail.com>
2017-06-30 09:48:13 -0500

    Latest updates.

--- www/controllers/tag.controller.js
+++ www/controllers/tag.controller.js
@@ -4,24 +4,43 @@ function TagController($scope, $state, UserService, $ionicPopup, $timeout) {
 
     $scope.checkTag = function() {
 
+        var stopLoop = false;
+
         for (var i = 0; i < 5; i++) {
 
             UserService.checkTag($scope.formData.tagName, $scope.formData.machineID, localStorage.userID).then(function(response) {
+                console.log("checkTag status: " + response.status);
                 if (response.status == "success") {
+                    console.log("Inside if statement.");
                     var alertPopup = $ionicPopup.alert({
-                            title: 'Tag has been registered.'
+                        title: 'Tag has been registered.'
                     });
-                    $state.go('tab.dash');
-                    break;
+                    console.log(1)
+                    stopLoop = true;
+                    console.log(2);
                 } 
                 else {
+                    console.log("Inside else statement.");
+                    console.log(3);
                     var alertPopup = $ionicPopup.alert({
-                            title: 'Tag has not been registered. Please attempt again.'
+                        title: 'Tag has not been registered. Please attempt again.'
                     });
-                } 
+                    console.log(4);
+                }
             })
 
-            $timeout(2000);
+            if (stopLoop) {
+                console.log(5);
+                $state.go('tab.dash');
+                console.log(6);
+                return;
+                console.log(7);
+            }
+            else {
+                console.log(8);
+                $timeout(5000);
+                console.log(9);
+            }
 
         }
 
