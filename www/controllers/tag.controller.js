285760ade4a477fe4f958fca397d359f07a10b45
Hamza Nauman <hn9@rice.edu>
2017-06-30 00:21:21 -0500

    The issue with the tags registration has been fixed

--- www/controllers/tag.controller.js
+++ www/controllers/tag.controller.js
@@ -1,32 +1,28 @@
-TagController.$inject = ['$scope', '$state', 'UserService', '$ionicPopup', '$timeout']
+TagController.$inject = ['$scope', '$state', 'UserService', '$ionicPopup', '$interval']
 
-function TagController($scope, $state, UserService, $ionicPopup, $timeout) {
+function TagController($scope, $state, UserService, $ionicPopup, $interval) {
+
+    $scope.registered = false;
 
     $scope.checkTag = function() {
-
-        for (var i = 0; i < 5; i++) {
-
-            UserService.checkTag($scope.formData.tagName, $scope.formData.machineID, localStorage.userID).then(function(response) {
-                if (response.status == "success") {
-                    var alertPopup = $ionicPopup.alert({
+        $interval(function() {
+            if (!$scope.registered) {
+                UserService.checkTag($scope.formData.tagName, $scope.formData.machineID, localStorage.userID).then(function(response) {
+                    if (response.status == "success") {
+                        var alertPopup = $ionicPopup.alert({
                             title: 'Tag has been registered.'
-                    });
-                    $state.go('tab.dash');
-                    break;
-                } 
-                else {
-                    var alertPopup = $ionicPopup.alert({
+                        });
+                        $scope.registered = true;
+                        $state.go('tab.dash');
+                    } else {
+                        var alertPopup = $ionicPopup.alert({
                             title: 'Tag has not been registered. Please attempt again.'
-                    });
-                } 
-            })
-
-            $timeout(2000);
-
-        }
-
+                        });
+                    } 
+                })
+            }
+        }, 2000, 5);
     }
-
 }
 
 module.exports = TagController;
